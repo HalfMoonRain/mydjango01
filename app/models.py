919fe8b5aa76cdb8714980433adb42d9bcad2325
JunWoo <junwoo0619@naver.com>
2022-08-16 11:55:57 +0900

    added app/index vies

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)  # 길이 제한이 있는 문자열
+    content = models.TextField()  # 길이 제한이 없는 문자열
